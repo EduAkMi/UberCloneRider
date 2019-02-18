3ae21e79db54251c2ec2a5c702f5a3d4b750142e
EduAkMi <edu_misina@hotmail.com>
2019-02-18 12:54:40 -0300

    13th video - Notification when Driver arrives

--- app/src/main/java/theron/uberclonerider/Service/MyFirebaseMessaging.java
+++ app/src/main/java/theron/uberclonerider/Service/MyFirebaseMessaging.java
@@ -1,24 +1,55 @@
 package theron.uberclonerider.Service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import theron.uberclonerider.R;
+
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(final RemoteMessage remoteMessage) {
-        //Because this is outside of Main thread, so if you want to run Toast,
-        // you need crete Handler to do that
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MyFirebaseMessaging.this, "" + remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        if (remoteMessage.getNotification().getTitle().equals("Cancel")) {
+            //Because this is outside of Main thread, so if you want to run Toast,
+            // you need crete Handler to do that
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MyFirebaseMessaging.this, "" + remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else if (remoteMessage.getNotification().getTitle().equals("Arrived")) {
+            showArrivedNotification(remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void showArrivedNotification(String body) {
+        //This code only work for Android API 25 and below
+        //From Android API 26 or higher, you need create Notification Channel
+        //I have publish tutorial about this content, you can watch on my channel
+        PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(),
+                0, new Intent(), PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext());
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_LIGHTS|Notification.DEFAULT_SOUND)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Arrived")
+                .setContentText(body)
+                .setContentIntent(contentIntent);
+        NotificationManager manager = (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, builder.build());
     }
 }
